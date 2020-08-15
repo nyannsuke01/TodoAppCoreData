68358d96c9957ce6bb76d8d9808cd6fe457d212f
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2020-08-16 05:55:38 +0900

    Initial Commit

---
+++ TodoAppCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoAppCoreData
+//
+//  Created by user on 2020/08/16.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
