73997483226ab59db3e7d415475ad6e5f9981af6
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2020-08-16 12:21:53 +0900

    coredataを用いたToDoアプリケーションの作成をしました。

--- TodoAppCoreData/ViewController.swift
+++ TodoAppCoreData/ViewController.swift
@@ -7,14 +7,81 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    var tasks : [Task] = []
+    
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        getData()
+        tableView.reloadData()
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        if task.isImportant {
+            cell.textLabel?.text = "👺" + task.name!
+        } else {
+            cell.textLabel?.text = task.name!
+        }
+        return cell
+    }
+
+    
+
+    func getData() {
+        //コンテキスト作成
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        do{
+            tasks = try context.fetch(Task.fetchRequest())
+        }
+        catch{
+            print("読み込み失敗")
+        }
+    }
+
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    //delete
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath:IndexPath){
+        //コンテキスト作成
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+
+        if editingStyle == .delete {
+
+            let task = tasks[indexPath.row]
+            context.delete(task)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+
+            do {
+                tasks = try context.fetch(Task.fetchRequest())
+            }
+            catch{
+                print("読み込み失敗！")
+            }
+        }
+        tableView.reloadData()
+    }
 }
 
